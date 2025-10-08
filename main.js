6d90d39710386a44b4892acebbb015a283658952
FEAR939 <94984067+FEAR939@users.noreply.github.com>
2025-10-09 00:03:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -222,6 +222,52 @@ export async function getBundle(url: string) {
   return episodes;
 }
 
+export async function getEpisode(source_hoster, episode) {
+  let sourceEpisode = false;
+  let bundleEpisodeNumber = -1;
+
+  if (source_hoster.episodes[0].isBundle) {
+    console.log("Episodes are bundled");
+    sourceEpisode = source_hoster.episodes.find((sourceEpisode) => {
+      const match = sourceEpisode.label.match(/E?(\d+)-E?(\d+)/);
+
+      const [bundleStart, bundleEnd] = match
+        ? [parseInt(match[1]), parseInt(match[2])]
+        : [0, 0];
+
+      const episodeNumber = parseInt(episode.episode);
+
+      bundleEpisodeNumber = episodeNumber - bundleStart + 1;
+
+      return bundleStart <= episodeNumber && bundleEnd >= episodeNumber;
+    });
+  } else {
+    sourceEpisode = source_hoster.episodes.find(
+      (sourceEpisode) =>
+        sourceEpisode.label.replace("Ep. ", "") == episode.episode,
+    );
+  }
+
+  if (!sourceEpisode) {
+    console.log("Episode not found");
+    return;
+  }
+
+  if (bundleEpisodeNumber === -1) {
+    const streamlink = await getEpisodeLink(sourceEpisode.url);
+
+    return streamlink;
+  }
+
+  const bundle = await getBundle(sourceEpisode.url);
+
+  const bundleEpisode = bundle![bundleEpisodeNumber - 1];
+
+  const streamlink = await getEpisodeLink(bundleEpisode.url);
+
+  return streamlink;
+}
+
 function levenshtein(a: string, b: string) {
   const an = a.length;
   const bn = b.length;
